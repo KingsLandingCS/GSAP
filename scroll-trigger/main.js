d66a89573f3befd77f4aaedcd2a987b37581f7f4
KingsLandingCS <finalfantasy.fantasy9@gmail.com>
2024-08-19 14:01:07 +0500

    learning

--- scroll-trigger/main.js
+++ scroll-trigger/main.js
@@ -1,24 +1,38 @@
-gsap.from("#page2 h1", {
+// gsap.from("#page2 h1", {
+//     opacity: 0,
+//     x: 500,
+//     duration: 2,
+//     scrollTrigger: {
+//         trigger: "#page2 h1",
+//         scroll: "body",
+//         markers: true,
+//         start: "top 50%"
+//     }
+// })
+
+// gsap.from("#page2 h2", {
+//     opacity: 0,
+//     x: -500,
+//     duration: 2,
+//     scrollTrigger: {
+//         trigger: "#page2 h1",
+//         scroll: "body",
+//         markers: true,
+//         start: "top 50%"
+//     }
+// })
+
+gsap.from("#page2 #box", {
+    scale: 0,
     opacity: 0,
-    x: 500,
-    duration: 2,
+    rotate: 720,
+    duration: 1,
     scrollTrigger: {
-        trigger: "#page2 h1",
-        scroll: "body",
+        trigger: "#page2 #box",
+        scroller: "body",
         markers: true,
-        start: "top 50%"
+        start: "top 60%",
+        end: "top 10%",
+        scrub: 2
     }
 })
-
-gsap.from("#page2 h2", {
-    opacity: 0,
-    x: -500,
-    duration: 2,
-    scrollTrigger: {
-        trigger: "#page2 h1",
-        scroll: "body",
-        markers: true,
-        start: "top 50%"
-    }
-})
-
