8ef9b5192827f28e700d00d3eeec3151aa8e53a2
KingsLandingCS <finalfantasy.fantasy9@gmail.com>
2024-08-19 13:51:51 +0500

    learning

--- scroll-trigger/main.js
+++ scroll-trigger/main.js
@@ -1,12 +1,24 @@
-gsap.from("#page1 #box", {
-    scale: 0,
-    delay: 1,
-    duration: 1.5,
-    rotate: 360,
+gsap.from("#page2 h1", {
+    opacity: 0,
+    x: 500,
+    duration: 2,
+    scrollTrigger: {
+        trigger: "#page2 h1",
+        scroll: "body",
+        markers: true,
+        start: "top 50%"
+    }
 })
-gsap.from("#page2 #box", {
-    scale: 0,
-    delay: 1,
-    duration: 1.5,
-    rotate: 360,
+
+gsap.from("#page2 h2", {
+    opacity: 0,
+    x: -500,
+    duration: 2,
+    scrollTrigger: {
+        trigger: "#page2 h1",
+        scroll: "body",
+        markers: true,
+        start: "top 50%"
+    }
 })
+
