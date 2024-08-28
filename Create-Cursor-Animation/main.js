f4724793fda9475ede4d8d68687c8b08100e72ab
KingsLandingCS <finalfantasy.fantasy9@gmail.com>
2024-08-28 07:36:29 +0500

    learning

--- Create-Cursor-Animation/main.js
+++ Create-Cursor-Animation/main.js
@@ -1,2 +1,11 @@
 var main = document.querySelector("#main");
 var cursor = document.querySelector("#cursor");
+
+main.addEventListener("mousemove", function (event) {
+    gsap.to(cursor, {
+        x: event.clientX,
+        y: event.clientY,
+        duration: 1,
+        ease: "back.out(2)"
+    })
+})
