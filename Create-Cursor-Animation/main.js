18deaa2dd1426b443ef0a1fb467b58ddcaff50fe
KingsLandingCS <finalfantasy.fantasy9@gmail.com>
2024-08-28 08:26:30 +0500

    learning

--- Create-Cursor-Animation/main.js
+++ Create-Cursor-Animation/main.js
@@ -1,11 +1,33 @@
 var main = document.querySelector("#main");
 var cursor = document.querySelector("#cursor");
+var imageDiv = document.querySelector("#image")
 
+// mouse move
 main.addEventListener("mousemove", function (event) {
     gsap.to(cursor, {
         x: event.clientX,
         y: event.clientY,
         duration: 1,
-        ease: "back.out(2)"
+        ease: "back.out(2)",
+        duration: 0.6,
+    })
+})
+
+// mouse enter
+imageDiv.addEventListener("mouseenter", function () {
+    cursor.innerHTML = "view More";
+    gsap.to(cursor, {
+        scale: 2,
+        backgroundColor: "#ffffff8a"
+    })
+})
+
+// mouse leave
+imageDiv.addEventListener("mouseleave", function () {
+    cursor.innerHTML = "";
+    gsap.to(cursor, {
+        scale: 1,
+        backgroundColor: "#fff"
+
     })
 })
